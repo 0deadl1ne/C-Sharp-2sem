cea5200d281a593c3224e5724fe03798e045cf75
0deadl1ne <46044294+0deadl1ne@users.noreply.github.com>
2020-03-24 22:07:23 +0200

    1, 2

---
+++ Press OK/Press OK/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Press_OK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(331, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Cancel);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(331, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Button2_MouseMove);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "OK";
+            this.Load += new System.EventHandler(this.Form);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Press OK/Press OK/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Press_OK
+{
+    public partial class Form1 : Form
+    {
+        private int k=1;
+        private bool VIS = false;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point cur = this.PointToClient(Cursor.Position);
+            Point loc = button2.Location;
+            int x_new = loc.X;
+            int y_new = loc.Y;
+
+           
+
+            if (cur.X < loc.X + 10) //движение вправо
+            {
+                x_new += 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(cur.X +10>loc.X + button2.Width) //движение влево
+            {
+                x_new += -30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (x_new < 30) //отступ слева
+            {
+                x_new = 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(x_new>this.Width - button2.Width - 30) //отступ справа
+            {
+                x_new = this.Width - button2.Width - 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (cur.Y < loc.Y + 10) //движение вниз
+            {
+                y_new += 30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(cur.Y + 10 > loc.Y + button2.Height) //движение вверх
+            {
+                y_new += -30;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new < 30) //отступ сверху
+            {
+                y_new = 10;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(y_new>this.Height - button2.Height - 30) //отступ снизу
+            {
+                y_new = this.Height - button2.Height - 45;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new > this.Height - button2.Height - 15) //курсор внизу формы
+            {
+                y_new = this.Height - button2.Height - 80;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if(x_new>this.Width - button2.Width - 15) //курсор справа формы
+            {
+                x_new += -100;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (y_new < 15) //курсор вверху формы
+            {
+                y_new += 50;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            if (x_new < 15) //курсор слева формы
+            {
+                x_new += 100;
+                button2.Width = button2.Width - 2;
+                button2.Height = button2.Height - 1;
+            }
+
+            button2.Location=new Point(x_new, y_new);
+        }
+        private void Form(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Cancel(object sender, EventArgs e)
+        {
+            this.Close();
+            button1.TabStop = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button2.TabStop = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (this.VIS)
+            {
+                this.Text = "Press OK button";
+                this.VIS = false;
+            }
+            else
+            {
+                this.Text = "";
+                this.VIS = true;
+            }
+            ++k;
+
+            if (this.k <= 16)
+                return;
+
+            this.timer1.Enabled = false;
+            this.VIS = false;
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+                if (this.VIS)
+                {
+                    this.Text = "OK will never been pressed";
+                    this.VIS = false;
+                }
+                else
+                {
+                    this.Text = "";
+                    this.VIS = true;
+                }
+                ++this.k;
+            
+        }
+    }
+}
